5a047f4c4c3b605b8100a4a1e6290a21735a322d
Scott Cox <jscox5@gmail.com>
2022-05-17 14:46:39 -0600

    initial commit with custom cell complete

---
+++ AddressBook/ViewControllers/ContactTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ContactTableViewCell.swift
+//  AddressBook
+//
+//  Created by Scott Cox on 5/17/22.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+
+
+
+} // End of Class
