af69ece6961ef9bec8ef140dd5f1628e429fd0cc
Scott Cox <jscox5@gmail.com>
2022-05-17 18:40:36 -0600

    custom cell set

--- AddressBook/ViewControllers/ContactTableViewCell.swift
+++ AddressBook/ViewControllers/ContactTableViewCell.swift
@@ -7,9 +7,45 @@
 
 import UIKit
 
+protocol ContactTableViewCellDelegate: AnyObject {
+    func toggleFavoriteButtonWasTapped(cell: ContactTableViewCell)
+}
+
 class ContactTableViewCell: UITableViewCell {
 
-
-
-
+    @IBOutlet weak var contactNameLabel: UILabel!
+    @IBOutlet weak var favoriteButton: UIButton!
+    
+    
+    
+    // MARK: Properties
+    
+    var person: Person? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+ 
+    weak var delegate: ContactTableViewCellDelegate?
+    
+    // MARK: - Helper Functions
+    func updateViews() {
+        guard let person = person else {return}
+        contactNameLabel.text = person.name
+        if person.isFavorite == true {
+            favoriteButton.setImage(UIImage(systemName: "star.fill"), for: .normal)
+        } else {
+            favoriteButton.setImage(UIImage(systemName: "star"), for: .normal)
+        }
+    }
+    
+    
+    
+    
+    // MARK: - Actions
+    @IBAction func favoriteButtonTapped(_ sender: Any) {
+    
+    }
+    
 } // End of Class
